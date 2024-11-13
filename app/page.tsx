b3da14d7228ab6246b70827859af03e672f6d8c3
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2024-11-13 11:57:37 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,135 @@
-import Image from "next/image";
+import { SourceTextModule } from "vm"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  /* Typescript Basics */
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  // Datatypes
+  const name = "" // Implicit Type = string (AVOID IMPLICIT TYPING)
+  const studentName: string = "Benny" // string
+  const studentAge: number = 15 // Int, Double, Floating-point
+  const studentIsEnrolled: boolean = true // boolean
+
+  /* Datatype + [] = array of datatype */
+  const scoreList: number[] = [] // Array of numbers
+  const nameCatalog: string[] = [] // Array of strings
+  const nameList: Array<string> = [] // Alternative Way to define an array
+
+  // Use of multiple types, can get complicated
+  let multipleDatatypes: [string, number]
+  multipleDatatypes = ["", 0]
+
+  // OR (Not forced to include BOTH datatypes)
+  // TODO - Why both datatypes?
+  let typeOrType: [string | number]
+  typeOrType = [""]
+
+  // Enum - Final Values
+  // Const enums - Ban?
+  enum Roles {
+    Guest,
+    User,
+    Admin,
+  }
+
+  // Without enums
+  const arvid: string = "Admin"
+  const linda: string = "Guest"
+  const linnea: string = "Gueest" // More probable to be incorrect
+
+  const benny: Roles = Roles.Guest // No Permission to View Content
+  const frida: Roles = Roles.User // Permission to View Content
+  const tomas: Roles = Roles.Admin // Permission to DELETE & View content
+  const lenny = Roles.Admin // AVOID IMPLICIT TYPING
+  // const lennart: Roles = "Roles.Admin" // Capture Error BEFORE running application
+
+  // const customValue = objectConversionFunction() // (User, UserDTO, SerializedUser, TimeDate, "", number)
+
+  // Enums are actually lists (arrays)
+  // By default, without initializing values, they get a value of 0 and above
+  enum WeatherConditions {
+    Sun = "SUN",
+    Rain = "RAIN",
+    Fog = "FOG",
+    Thunder = "THUNDER",
+    Storm = "STORM",
+    Tornado = "TORNADO",
+  }
+
+  const todaysWeather: WeatherConditions = WeatherConditions.Fog
+
+  // Loop through an enum
+  function iterateThroughWeathers() {
+    Object.keys(WeatherConditions).map((weather) => console.log(weather))
+  }
+
+  iterateThroughWeathers()
+
+  enum Month {
+    January,
+    February,
+    March,
+    April,
+  }
+
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+  }
+
+  enum Queries {
+    Select,
+    From,
+    Where,
+  }
+
+  // Why does ANY exist? A: For JavaScript to Typescript refactoring
+  const IDontKnow: any = ""
+  const IDontKnow2: any = 0
+
+  // No return = void (can be avoided during function creation)
+  function sayHello(): void {
+    console.log("Hello world")
+  }
+
+  // callback (a function, calls another function)
+  function algorithm(algorithm: () => void) {}
+  // algorithm(console.log())
+
+  // Objects (Variables can become objects)
+  let customUser: {
+    username: string
+    password: string
+    printDetails: () => void
+  } = {
+    username: "",
+    password: "",
+    printDetails: function (): void {
+      console.log(this.username, this.password)
+    },
+  }
+
+  customUser.printDetails()
+
+  // interface
+  interface CustomUser {
+    username: string
+    password: string
+    printDetails: () => void
+  }
+
+  // Interface Approach
+  const customUser2: CustomUser = {
+    username: "benny",
+    password: "123",
+    printDetails: function (): void {
+      throw new Error("Function not implemented.")
+    },
+  }
+
+  const userList: CustomUser[] = [customUser2]
+  const userList2: Array<CustomUser> = [customUser2]
+
+  return <div></div>
 }
